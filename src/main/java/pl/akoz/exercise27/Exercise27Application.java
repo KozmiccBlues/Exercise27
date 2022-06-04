cbe74285a7b83faa3212cadad3ce1f9940a8ed40
Ola <kozlowska.aleksandra92@gmail.com>
2022-06-04 15:37:26 +0200

    start

---
+++ src/main/java/pl/akoz/exercise27/Exercise27Application.java
@@ -0,0 +1,13 @@
+package pl.akoz.exercise27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise27Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akoz/exercise27/Exercise27ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akoz.exercise27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
